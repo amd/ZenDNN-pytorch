774e29d97d616689344270da288f41b0a61069ff
Richard Zou <zou3519@gmail.com>
2021-04-21 09:18:53 -0700

    TORCH_SHOW_DISPATCH_TRACE

--- aten/src/ATen/core/dispatch/Dispatcher.h
+++ aten/src/ATen/core/dispatch/Dispatcher.h
@@ -11,8 +11,11 @@
 #include <c10/util/LeftRight.h>
 #include <mutex>
 #include <list>
+#include <iostream>
 
 #include <ATen/core/grad_mode.h>
+
+#define HAS_TORCH_SHOW_DISPATCH_TRACE
 
 #if C10_MOBILE
 #define C10_DISPATCHER_INLINE_UNLESS_MOBILE inline
@@ -23,6 +26,8 @@ namespace c10 {
 
 class TORCH_API OperatorHandle;
 template<class FuncType> class TypedOperatorHandle;
+
+bool TORCH_API show_dispatch_trace_enabled();
 
 /**
  * Implement this interface and register your instance with the dispatcher
@@ -501,6 +506,9 @@ inline Return Dispatcher::callWithDispatchKeySlowPath(const TypedOperatorHandle<
     // Note: for perf reasons we wouldn't want to pass arguments into
     // the function call or prematurely box them
   at::RecordFunction guard(at::RecordScope::FUNCTION, pre_sampled);
+  if (show_dispatch_trace_enabled()) {
+    std::cout << "[" << op.schema().name() << "] " << dispatchKeySet.highestPriorityTypeId() << " (::callWithDispatchKeySlowPath)" << std::endl;
+  }
   if (C10_UNLIKELY(guard.isActive())) {
     auto dispatchKey = dispatchKeySet.highestPriorityTypeId();
     if (op.operatorDef_->op.isObserved()) {
@@ -531,6 +539,9 @@ C10_DISPATCHER_INLINE_UNLESS_MOBILE Return Dispatcher::call(const TypedOperatorH
   auto dispatchKeySet = op.operatorDef_->op.dispatchKeyExtractor()
     .template getDispatchKeySetUnboxed<Args...>(args...);
   TORCH_INTERNAL_ASSERT_DEBUG_ONLY(!c10::isAliasDispatchKey(dispatchKeySet.highestPriorityTypeId()));
+  if (show_dispatch_trace_enabled()) {
+    std::cout << "[" << op.schema().name() << "] " << dispatchKeySet.highestPriorityTypeId() << " (::call)" << std::endl;
+  }
   const KernelFunction& kernel = op.operatorDef_->op.lookup(dispatchKeySet.highestPriorityTypeId());
 #ifndef PYTORCH_DISABLE_PER_OP_PROFILING
   // By default, when there're no high-frequency or non-sampled callbacks,
@@ -553,6 +564,9 @@ inline Return Dispatcher::redispatch(const TypedOperatorHandle<Return (Args...)>
   detail::unused_arg_(args...);  // workaround for a false-positive warning about unused parameters in gcc 5
   // do not use RecordFunction on redispatch
   const KernelFunction& kernel = op.operatorDef_->op.lookup(currentDispatchKeySet.highestPriorityTypeId());
+  if (show_dispatch_trace_enabled()) {
+    std::cout << "[" << op.schema().name() << "] " << currentDispatchKeySet.highestPriorityTypeId() << " (::redispatch)" << std::endl;
+  }
   return kernel.template call<Return, Args...>(op, currentDispatchKeySet, std::forward<Args>(args)...);
 }
 
@@ -560,6 +574,10 @@ inline void Dispatcher::callBoxed(const OperatorHandle& op, Stack* stack) const 
   // note: this doesn't need the mutex because write operations on the list keep iterators intact.
   const auto& entry = op.operatorDef_->op;
   auto dispatchKeySet = entry.dispatchKeyExtractor().getDispatchKeySetBoxed(stack);
+  if (show_dispatch_trace_enabled()) {
+    std::cout << dispatchKeySet << std::endl;
+    std::cout << "[" << op.schema().name() << "] " << dispatchKeySet.highestPriorityTypeId() << " (::callBoxed)" << std::endl;
+  }
   const auto& kernel = entry.lookup(dispatchKeySet.highestPriorityTypeId());
 #ifndef PYTORCH_DISABLE_PER_OP_PROFILING
   bool pre_sampled = false;
@@ -592,6 +610,9 @@ inline void Dispatcher::callBoxed(const OperatorHandle& op, Stack* stack) const 
 inline void Dispatcher::redispatchBoxed(const OperatorHandle& op, DispatchKeySet dispatchKeySet, Stack* stack) const {
   // note: this doesn't need the mutex because write operations on the list keep iterators intact.
   const auto& entry = op.operatorDef_->op;
+  if (show_dispatch_trace_enabled()) {
+    std::cout << "[" << op.schema().name() << "] " << dispatchKeySet.highestPriorityTypeId() << " (::redispatchBoxed)" << std::endl;
+  }
   const auto& kernel = entry.lookup(dispatchKeySet.highestPriorityTypeId());
   return kernel.callBoxed(op, dispatchKeySet, stack);
 }
