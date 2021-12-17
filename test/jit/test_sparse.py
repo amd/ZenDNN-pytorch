8926f706e933ba11ccfc78cac7257f0bc8549e12
David Berard <dberard@fb.com>
2021-12-16 20:46:26 -0800

    [JIT] scripting, freezing, serialization for sparse csr
    1. Implement pickling/unpickling
    2. Add `test_freeze_sparse_csr, tests_serialize_sparse_csr` tests
    
    ghstack-source-id: 51beadaee433cb125e6f160f47762c9a144ad884
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/69555

--- test/jit/test_sparse.py
+++ test/jit/test_sparse.py
@@ -59,3 +59,58 @@ class TestSparse(JitTestCase):
         loaded_result = loaded_model.forward(x)
 
         self.assertEqual(expected_result, loaded_result)
+
+    def test_freeze_sparse_csr(self):
+        class SparseTensorModule(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.a = torch.rand(3, 4).to_sparse_csr()
+                self.b = torch.rand(3, 4).to_sparse_csr()
+
+
+            def forward(self, x):
+                return x + self.a + self.b
+
+        x = torch.rand(3, 4).to_sparse_csr()
+
+        m = SparseTensorModule()
+        unfrozen_result = m.forward(x)
+
+        m.eval()
+        frozen = torch.jit.freeze(torch.jit.script(m))
+
+        frozen_result = frozen.forward(x)
+
+        self.assertEqual(unfrozen_result.to_dense(), frozen_result.to_dense())
+
+        buffer = io.BytesIO()
+        torch.jit.save(frozen, buffer)
+        buffer.seek(0)
+        loaded_model = torch.jit.load(buffer)
+
+        loaded_result = loaded_model.forward(x)
+
+        self.assertEqual(unfrozen_result.to_dense(), loaded_result.to_dense())
+
+    def test_serialize_sparse_csr(self):
+        class SparseTensorModule(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.a = torch.rand(3, 4).to_sparse_csr()
+                self.b = torch.rand(3, 4).to_sparse_csr()
+
+            def forward(self, x):
+                return x + self.a + self.b
+
+        x = torch.rand(3, 4).to_sparse_csr()
+        m = SparseTensorModule()
+        expected_result = m.forward(x)
+
+        buffer = io.BytesIO()
+        torch.jit.save(torch.jit.script(m), buffer)
+        buffer.seek(0)
+        loaded_model = torch.jit.load(buffer)
+
+        loaded_result = loaded_model.forward(x)
+
+        self.assertEqual(expected_result.to_dense(), loaded_result.to_dense())
