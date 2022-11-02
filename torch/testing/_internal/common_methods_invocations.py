678c8c65b4bb02daff60483ae7fbd59444041b6a
Sean Ross-Ross <srossross@gmail.com>
2022-11-02 15:47:30 -0500

    added new inputs and error input

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -4418,9 +4418,12 @@ def sample_inputs_narrow_narrow_copy(op_info, device, dtype, requires_grad, *, i
         if is_narrow:
             yield SampleInput(tensor, dim, torch.tensor(start), length)
 
+
 def sample_inputs_view_copy(op_info, device, dtype, requires_grad, **kwargs):
     shapes = (
+        ((S, S), [S * S, 1]),
         ((S, S, S), [S * S, S]),
+        ((S, S, S), [S * S * S]),
     )
 
     for shape, new_shape in shapes:
@@ -4429,107 +4432,21 @@ def sample_inputs_view_copy(op_info, device, dtype, requires_grad, **kwargs):
         yield SampleInput(tensor, new_shape)
 
 
-    shapes_and_args = (
-        # 1-dim
-        ((M,), 0, 0, 0),    # 0 elems from the left
-        ((M,), -1, -1, 0),  # 0 elems from the right
-        ((M,), 0, 5, 3),    # 3 elems from the left
-        ((M,), 0, -5, 2),   # 2 elems from the right
-        ((M,), -1, 0, M),   # M elems from the left
-        ((M,), 0, -M, M),   # M elems from the right
+def error_inputs_view_copy(op_info, device):
+    tensor = make_tensor([S, S], dtype=torch.float32, device=device)
+    # Size is too big
+    s0 = SampleInput(tensor, [S * S * 10])
+    yield ErrorInput(s0, error_regex="shape '\[250\]' is invalid for input of size 25", error_type=RuntimeError)
 
-        # 2-dim
-        ((M, S), 1, 0, 0),    # dim 1, 0 elems from the left
-        ((S, M), -2, -1, 0),  # dim 0, 0 elems from the right
-        ((L, S), 1, 2, 3),    # dim 1, 3 elems from the left
-        ((L, S), -1, 3, 2),   # dim 1, 2 elems from the left
-        ((M, L), 0, 0, M),    # dim 0, M elems from the left
-        ((M, L), -1, -L, L),  # dim 1, L elems from the right
 
-        # 3-dim
-        ((L, M, S), 2, 0, 0),    # dim 2, 0 elems from the left
-        ((M, S, L), -1, -1, 0),  # dim 2, 0 elems from the right
-        ((S, L, M), 2, 0, M),    # dim 2, M elems from the left
-        ((L, S, M), -1, -M, M),  # dim 2, M elems from the right
-        ((S, L, M), 1, 0, 0),    # dim 1, 0 elems from the left
-        ((S, L, M), 0, 2, 1),    # dim 0, 1 elem from the left
-        ((M, S, M), -1, -5, 4),  # dim 2, 4 elems from the right
-    )
-
-    for shape, dim, start, length in shapes_and_args:
-        tensor = make_tensor(shape, dtype=dtype, device=device, low=None, high=None,
-                             requires_grad=requires_grad)
-        yield SampleInput(tensor, dim, start, length)
-        # narrow also accepts the start argument being a Tensor
-        if is_narrow:
-            yield SampleInput(tensor, dim, torch.tensor(start), length)
-
-def error_inputs_narrow_narrow_copy(op_info, device, *, is_narrow, is_ref):
-    make_arg = partial(make_tensor, device=device, dtype=torch.float32)
-
-    # 0-dim
-    yield ErrorInput(SampleInput(make_arg(()), 0, 0, 1),
-                     error_type=RuntimeError,
-                     error_regex=r"narrow\(\) cannot be applied to a 0-dim tensor\.")
-
-    # out of bounds dim
-    if not is_narrow and not is_ref and torch.device(device).type == 'cpu':
-        # narrow_copy_dense_cpu_out
-        yield ErrorInput(SampleInput(make_arg((M, S, L)), 3, 0, 0),
-                         error_type=RuntimeError,
-                         error_regex=r"Expected dim < static_cast<int64_t>\(self_sizes.size\(\)\) to be true, but got false\.")
-    else:
-        yield ErrorInput(SampleInput(make_arg((M, S, L)), 3, 0, 0),
-                         error_type=IndexError,
-                         error_regex=r"Dimension out of range \(expected to be in range of \[-3, 2\], but got 3\)")
-    # out of bounds dim (negative)
-    yield ErrorInput(SampleInput(make_arg((L, S, M)), -4, 0, 0),
-                     error_type=IndexError,
-                     error_regex=r"Dimension out of range \(expected to be in range of \[-3, 2\], but got -4\)")
-
-    # out of bounds start
-    if not is_narrow and not is_ref and torch.device(device).type == 'cpu':
-        # narrow_copy_dense_cpu_out
-        yield ErrorInput(SampleInput(make_arg((L, M, S)), 1, M + 1, 0),
-                         error_type=RuntimeError,
-                         error_regex=r"start \(11\) \+ length \(0\) exceeds dimension size \(10\)\.")
-    else:
-        yield ErrorInput(SampleInput(make_arg((L, M, S)), 1, M + 1, 0),
-                         error_type=IndexError,
-                         error_regex=r"Dimension out of range \(expected to be in range of \[-10, 9\], but got 11\)")
-    # out of bounds start (negative)
-    yield ErrorInput(SampleInput(make_arg((L, M, S)), 1, -M - 1, 0),
-                     error_type=IndexError,
-                     error_regex=r"Dimension out of range \(expected to be in range of \[-10, 9\], but got -11\)")
-
-    # out of bounds length
-    yield ErrorInput(SampleInput(make_arg((S, L, M)), 2, 0, M + 1),
-                     error_type=RuntimeError,
-                     error_regex=r"start \(0\) \+ length \(11\) exceeds dimension size \(10\)\.")
-    # out of bounds length (negative)
-    if not is_narrow and not is_ref and torch.device(device).type == 'cpu':
-        # narrow_copy_dense_cpu_out
-        yield ErrorInput(SampleInput(make_arg((M,)), 0, 0, -1),
-                         error_type=RuntimeError,
-                         error_regex=r"start \(0\) \+ length \(-1\) exceeds dimension size \(10\)\.")
-    else:
-        yield ErrorInput(SampleInput(make_arg((M,)), 0, 0, -1),
-                         error_type=RuntimeError,
-                         error_regex=r"narrow\(\): length must be non-negative\.")
-
-    # Test Tensor overload that was added for XLA. Start must be an 0-dim
-    # integral Tensor. narrow_copy doesn't have this overload.
-    # https://github.com/pytorch/pytorch/issues/31558
-    if is_narrow:
-        # *1-dim* integral Tensor
-        yield ErrorInput(SampleInput(make_arg((L, M, S)), 1, make_arg(S, dtype=torch.int), 2),
-                         error_type=RuntimeError,
-                         error_regex=r"start must be an 0-dim integral Tensor\.")
-
-        # 0-dim *bool* Tensor (bools are not allowed)
-        yield ErrorInput(SampleInput(make_arg((L, M, S)), -3, make_arg((), dtype=torch.bool), 3),
-                         error_type=RuntimeError,
-                         error_regex=r"start must be an 0-dim integral Tensor\.")
+def sample_inputs_narrow(op_info, device, dtype, requires_grad, **kwargs):
+    '''
+    sample_inputs_narrow accepts the same inputs as narrow_copy, in addition
+    narrow also accepts `start` argument to be a Tensor.
+    '''
+    for sample in sample_inputs_narrow_copy(op_info, device, dtype, requires_grad, **kwargs):
+        yield sample
+        yield SampleInput(sample.input, args=(sample.args[0], torch.tensor(sample.args[1]), sample.args[2]))
 
 
 def sample_trapezoid(op_info, device, dtype, requires_grad, **kwargs):
@@ -12607,6 +12524,7 @@ op_db: List[OpInfo] = [
            supports_fwgrad_bwgrad=True,
            supports_autograd=True,
            sample_inputs_func=sample_inputs_view_copy,
+           error_inputs_func=error_inputs_view_copy,
            skips=(
                # view_copy does not support automatic differentiation for outputs with complex dtype
                DecorateInfo(unittest.expectedFailure, 'TestCommon', 'test_dtypes'),
