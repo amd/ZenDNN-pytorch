97c9198e04e9e3cde241444856a8a644604b39d2
mikey dagitses <mikeyd@meta.com>
2022-12-06 08:49:06 -0500

    set -Wsuggest-override for builds
    Summary: This was flagged by a Meta internal build.
    
    Test Plan: Rely on CI.

--- aten/src/ATen/native/cudnn/Conv_v8.cpp
+++ aten/src/ATen/native/cudnn/Conv_v8.cpp
@@ -8,7 +8,12 @@
 #if HAS_CUDNN_V8()
 
 #include <ATen/cudnn/cudnn-wrapper.h>
+
+#pragma GCC diagnostic push
+C10_SUPPRESS_WARNING_IF_DEFINED("-Wsuggest-override")
 #include <cudnn_frontend.h>
+#pragma GCC diagnostic pop
+
 #include <cudnn_frontend_find_plan.h>
 #include <cudnn_frontend_get_plan.h>
 #include <ATen/core/Tensor.h>
