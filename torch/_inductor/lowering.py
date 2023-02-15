69d10ea776f7c92a533abee6553f273297e88bb2
Liao, Xuan <xuan.liao@intel.com>
2023-02-15 14:03:14 +0000

    [inductor] fix pool2d with ceil mode

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -2697,7 +2697,9 @@ def pooling_size(x, i, kernel_size, stride, padding, ceil_mode):
         x_alt = ir.FloorDiv(
             x + 2 * padding[i] - (kernel_size[i] - 1) + 2 * (stride[i] - 1), stride[i]
         )
-
+        if V.graph.sizevars.size_hint((x_alt - 1) * stride[i] - x - padding[i]) >= 0:
+            # Sliding windows must start within the input or left padding
+            x_alt -= 1
         if V.graph.sizevars.size_hint(x_out - x_alt) == 0:
             # ceil mode is actually a no-op, lets guard on that
             V.graph.sizevars.guard_equals(x_out, x_alt)
