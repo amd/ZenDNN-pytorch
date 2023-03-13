142f3ab17670e6413f1c4f894657b56f7d21a6f8
Zachary DeVito <zdevito@meta.com>
2023-03-12 23:00:42 -0700

    [memory profiling] add stack traces to C++ API
    This adds support for stack trace gathering in memory profiling that
    happens directly from C++. It records stacks with both TorchScript and
    C++ frames.
    
    ghstack-source-id: cb951eaf960c00061337f0e464628eb6d2fa4c9a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/95541

--- torch/csrc/profiler/unwind/unwind.h
+++ torch/csrc/profiler/unwind/unwind.h
@@ -1,3 +1,4 @@
+#include <c10/macros/Export.h>
 #include <string>
 #include <vector>
 
@@ -5,7 +6,7 @@ namespace torch {
 namespace unwind {
 // gather current stack, relatively fast.
 // gets faster once the cache of program counter locations is warm.
-std::vector<void*> unwind();
+TORCH_CUDA_CU_API std::vector<void*> unwind();
 
 struct Frame {
   std::string filename;
@@ -19,7 +20,8 @@ struct Frame {
 // Callers should first batch up all the unique void* pointers
 // across a number of unwind states and make a single call to
 // symbolize.
-std::vector<Frame> symbolize(const std::vector<void*>& frames);
+TORCH_CUDA_CU_API std::vector<Frame> symbolize(
+    const std::vector<void*>& frames);
 
 struct Stats {
   size_t hits = 0;
