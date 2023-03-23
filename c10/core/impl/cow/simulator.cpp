d69be1839c2b72948e7a2562dec1ef5962059cdf
mikey dagitses <mikeyd@meta.com>
2023-03-23 00:34:56 -0700

    warn on future reshape alias mutation violations
    Summary:
    To have the highest fidelity warning, we wish to track contemporary
    modifications to view tensors created by reshape that in the future
    will be lazy copies.
    
    We believe that we can track this "perfectly" with a generation number
    attached to tensors and storages.
    
    Test Plan: Added new tests.
    
    Reviewers: ezyang
    
    Subscribers:
    
    Tasks:
    
    Tags:

---
+++ c10/core/impl/cow/simulator.cpp
@@ -0,0 +1,28 @@
+#include <c10/core/impl/cow/simulator.h>
+
+#include <cassert>
+#include <limits>
+
+namespace c10::impl::cow {
+
+template <bool intrusive>
+SimulatorImpl<intrusive>::SimulatorImpl(std::uint64_t storage_generation) noexcept
+      : storage_generation_(storage_generation) {}
+
+template <bool intrusive>
+auto SimulatorImpl<intrusive>::storage_generation() noexcept -> std::uint64_t {
+  std::lock_guard<std::mutex> lock(mtx_);
+  return storage_generation_;
+}
+
+template <bool intrusive>
+auto SimulatorImpl<intrusive>::bump_storage_generation() noexcept -> std::uint64_t {
+  std::lock_guard<std::mutex> lock(mtx_);
+  assert(storage_generation_ != std::numeric_limits<std::uint64_t>::max());
+  return ++storage_generation_;
+}
+
+template class SimulatorImpl<true>;
+template class SimulatorImpl<false>;
+
+} // namespace c10::impl::cow
