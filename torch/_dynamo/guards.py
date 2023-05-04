b3afc8ebd34dfada2a62582480135652eda92374
Sam Gross <colesbury@gmail.com>
2023-05-04 18:45:23 +0000

    [dynamo] Guard on nn.Module dicts and type
    The NN_MODULE guard now subsumes guards on Module attributes. The
    check_fn will fail if the module attributes are changed (such as
    Module.training), parameters, submodules, and buffers are added or
    removed, and if fields are changed on the type itself.
    
    This gives up specificity in the guard check -- if any field is changed
    the check_fn fails -- for faster overall checks.

--- torch/_dynamo/guards.py
+++ torch/_dynamo/guards.py
@@ -340,19 +340,14 @@ class GuardBuilder(GuardBuilderBase):
             self.EQUALS_MATCH(guard)
 
     def NN_MODULE(self, guard: Guard):
-        self.ID_MATCH(guard)
         ref = self.arg_ref(guard)
         val = self.get(guard.name)
-
-        def setup_guard():
-            assert istype(val.training, bool)
-            self.code.append(f"{ref}.training == {val.training}")
-
-        if hasattr(val, "training"):
-            # There are cases where a monkeypatched object has a guard made between __new__ and __init__
-            setup_guard()
-        else:
-            unimplemented(f"Guard setup for uninitialized class {type(val)}")
+        # The module guard checks for modifications to the Module's type, __dict__,
+        # and various nested OrderedDicts, such as _parameters, _buffers, and _modules.
+        # This subsumes the check for Module.training.
+        g = torch._C._dynamo.guards.nn_module_guard(val)
+        name = self.check_fn_manager.add_extra_closure_var("__nn_module_guard", g)
+        self._produce_guard_code(guard, [f"{name}()"])
 
     def FUNCTION_MATCH(self, guard: Guard):
         """things like torch.add and user defined functions"""
@@ -654,6 +649,7 @@ class CheckFunctionManager:
         self.valid = True
         self._weakrefs: List["ReferenceType[object]"] = []
         self._seen_ids: Set[int] = set()
+        self._extra_closure_vars: Dict[str, object] = {}
         self.output_graph = output_graph
 
         # Note: right overrides left
@@ -702,6 +698,7 @@ class CheckFunctionManager:
                 and "__defaults__" not in guard.name
                 and "__kwdefaults__" not in guard.name
                 and (config.skip_nnmodule_hook_guards or "hooks" not in guard.name)
+                and guard.create_fn is not GuardBuilder.NN_MODULE  # Force-enable NN_MODULE checks for now
             ):
                 continue
             guard.create(local_builder, global_builder)
@@ -814,6 +811,7 @@ class CheckFunctionManager:
             + list(SYMPY_INTERP.items())
         )
         closure_vars.update(CLOSURE_VARS)
+        closure_vars.update(self._extra_closure_vars)
         py_code = f"""\
 def ___make_guard_fn({','.join(closure_vars.keys())}):
     return lambda L: {code}
@@ -847,6 +845,14 @@ def ___make_guard_fn({','.join(closure_vars.keys())}):
         except TypeError:
             pass  # cannot weakref bool object
         return id(obj)
+
+    def add_extra_closure_var(self, name_hint, obj):
+        idx = 0
+        while f"{name_hint}_{idx}" in self._extra_closure_vars:
+            idx += 1
+        name = f"{name_hint}_{idx}"
+        self._extra_closure_vars[name] = obj
+        return name
 
 
 stashed_first_fail_reason = None
