f1173b2f959b7ec71bd422d5ef6da1130ac82598
Will Constable <whc@fb.com>
2023-05-09 04:10:22 +0000

    Train Step Compile [1/3] basic fwd/bwd only
    Adds infra to wrap compilers in TrainStepCompiler and logic to dynamo
    to behave specially (e.g. allowing .backward() to not graph-break)
    when using such compilers.
    
    Integrates some state with TracingContext, useful for both asserting
    only one call to .backard() as well as groundwork for optimizer support
    in following PR.
    
    Checks various assumptions about the user trainstep function:
     - only one call to .backward() - double backwards is uncommon, and
       initial users of train step compile are expected to be satisfied
       without it.  Not sure if it would be feasible to support.
     - only supported backends are allowed
     - fullgraph (nopython) is required when train_step_compiling
    
    Separated PRs for easier review, but this is of minimal utility
    on its own without optimizer support.
    
    ghstack-source-id: 91140ca67de02e8f41413969e5201c2bbeabcace
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/100605

--- torch/_guards.py
+++ torch/_guards.py
@@ -365,6 +365,39 @@ class GuardsContext(Checkpointable[GuardsCheckpointState]):
         self.dynamo_guards = state.dynamo_guards
 
 
+class TrainStepCheckpointState:
+    backward_called: bool = False
+
+    def __init__(self, backward_called):
+        self.backward_called = backward_called
+
+    def diff(self, other):
+        if self.backward_called != other.backward_called:
+            return set("backward_called")
+        return None
+
+    def __eq__(self, other):
+        return self.diff(other) is None
+
+
+class TrainStepContext(Checkpointable[TrainStepCheckpointState]):
+    """
+    Tracks state across dynamo tracing and train-step compiler,
+    mostly to enforce safety invariants.
+    """
+
+    def __init__(self):
+        # only support one backward call
+        self.backward_called = False
+
+    def copy_graphstate(self):
+        return TrainStepCheckpointState(self.backward_called)
+
+    def restore_graphstate(self, state):
+        assert isinstance(state, TrainStepCheckpointState)
+        self.backward_called = state.backward_called
+
+
 _CURRENT_TRACING_CONTEXT = None
 
 """
@@ -392,6 +425,9 @@ class TracingContext:
 
     Note that it is a staticmethod, and invocations outside of `with tracing()` (see below), are valid but
     will return None.
+
+    Everything on TracingContext must support implement `Checkpointable` interface, so it will properly
+    react to Dynamo rolling back and restoring context.
     """
 
     @staticmethod
@@ -404,6 +440,7 @@ class TracingContext:
         self.fake_mode = fake_mode
         self.frame_summary_stack = []
         self.loc_in_frame = None
+        self.trainstep: Optional[TrainStepContext] = None
 
     @staticmethod
     def extract_stack():
@@ -449,6 +486,31 @@ class TracingContext:
             tc is not None
         ), "Loc context manager must be called within an ongoing trace."
         tc.loc_in_frame = traceback.FrameSummary(filename, lineno, frame_name)
+
+    @staticmethod
+    def trace_train_step():
+        """
+        Mark that we're tracing a train step (no going back).
+        """
+        tc = TracingContext.get()
+        assert (
+            tc is not None
+        ), "trace_train_step() must be called within an ongoing trace."
+        tc.trainstep = TrainStepContext()
+
+    @staticmethod
+    def train_step_context(assert_if_missing=False):
+        """
+        Get the train step context if active, or None. Optionally, assert.
+        """
+        tc = TracingContext.get()
+        assert (
+            tc is not None
+        ), "train_step_context() must be called within an ongoing trace."
+        if assert_if_missing:
+            assert tc.trainstep, "Expected an active train_step context but found none"
+
+        return tc.trainstep
 
 
 """
