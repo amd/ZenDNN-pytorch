bbcf615ff59a159ba769b93c65ff82fbc5434c12
William Wen <williamwen@meta.com>
2023-05-30 10:37:19 -0700

    Update test.sh

--- .ci/pytorch/test.sh
+++ .ci/pytorch/test.sh
@@ -427,8 +427,6 @@ test_dynamo_benchmark() {
   shift
   local shard_id="$1"
   shift
-  
-  python -m pip install patch transformers
 
   if [[ "${TEST_CONFIG}" == *perf_compare* ]]; then
     test_single_dynamo_benchmark "training" "$suite" "$shard_id" --training --amp "$@"
