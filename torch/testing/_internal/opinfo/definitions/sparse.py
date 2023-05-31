0d5d91f657a5b3c8d0c4cc2aeb8825df298bd002
yanbing-j <yanbing.jiang@intel.com>
2023-05-31 14:49:33 +0800

    Fix CI in sparse.py

--- torch/testing/_internal/opinfo/definitions/sparse.py
+++ torch/testing/_internal/opinfo/definitions/sparse.py
@@ -323,44 +323,45 @@ def _validate_sample_input_sparse_reduction_sum(sample, check_validate=False):
     dim = t_kwargs.get("dim")
     keepdim = t_kwargs.get("keepdim")
     layout = t_inp.layout
-    if layout in {
-        torch.sparse_csr,
-        torch.sparse_csc,
-        torch.sparse_bsr,
-        torch.sparse_bsc,
-    }:
-        if layout in {torch.sparse_csc, torch.sparse_bsr, torch.sparse_bsc}:
-            return ErrorInput(
-                sample,
-                error_regex=(
-                    "sum expected input with strided, sparse_csr layouts, got layout"
-                ),
-            )
-        if layout == torch.sparse_csr and not keepdim:
-            return ErrorInput(
-                sample,
-                error_regex=(
-                    "reduction operations on CSR tensors with keepdim=False is unsupported"
-                ),
-            )
-        if t_inp.dim() != 2:
-            return ErrorInput(
-                sample,
-                error_regex=("input_dim == 2 INTERNAL ASSERT"),
-            )
-        if layout == torch.sparse_csr:
-            if t_inp.dtype == torch.bool:
-                return ErrorInput(
-                    sample,
-                    error_regex=("_sparse_csr_sum_cpu not implemented for 'Bool'"),
-                )
-            if t_inp.dtype == torch.complex32:
+    if (isinstance(dim, int) or isinstance(dim, (list, tuple))):
+        if layout in {
+            torch.sparse_csr,
+            torch.sparse_csc,
+            torch.sparse_bsr,
+            torch.sparse_bsc,
+        }:
+            if layout in {torch.sparse_csc, torch.sparse_bsr, torch.sparse_bsc}:
                 return ErrorInput(
                     sample,
                     error_regex=(
-                        "_sparse_csr_sum_cuda not implemented for 'ComplexHalf'"
+                        "sum expected input with strided, sparse_csr layouts, got layout"
                     ),
                 )
+            if layout in {torch.sparse_csr, torch.sparse_csc} and not keepdim:
+                return ErrorInput(
+                    sample,
+                    error_regex=(
+                        "reduction operations on CSR tensors with keepdim=False is unsupported"
+                    ),
+                )
+            if t_inp.dim() != 2:
+                return ErrorInput(
+                    sample,
+                    error_regex=("input_dim == 2 INTERNAL ASSERT"),
+                )
+            if layout == torch.sparse_csr:
+                if t_inp.dtype == torch.bool:
+                    return ErrorInput(
+                        sample,
+                        error_regex=("_sparse_csr_sum_cpu not implemented for 'Bool'"),
+                    )
+                if t_inp.dtype == torch.complex32:
+                    return ErrorInput(
+                        sample,
+                        error_regex=(
+                            "_sparse_csr_sum_cuda not implemented for 'ComplexHalf'"
+                        ),
+                    )
     return sample
 
 
