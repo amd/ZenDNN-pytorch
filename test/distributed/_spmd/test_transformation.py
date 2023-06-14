fe49279d21e96f15f6f82c580f48753a3401f640
Chien-Chin Huang <chienchin@fb.com>
2023-06-14 16:39:41 -0700

    [SPMD][BE] Split graph optimization module into multiple submodules
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/101012
    
    Graph optimization module is becoming too large for efficient development. This PR splits it into smaller ones. This PR also changes the naming of graph optimizations as not all passes target the performance.
    ghstack-source-id: 192251597
    
    Differential Revision: [D45703835](https://our.internmc.facebook.com/intern/diff/D45703835/)

--- test/distributed/_spmd/test_transformation.py
+++ test/distributed/_spmd/test_transformation.py
@@ -10,8 +10,7 @@ import torch.nn as nn
 from torch._inductor.utils import has_triton
 from torch.distributed._spmd.api import compile
 from torch.distributed._spmd.gm_transformation import GraphModuleTransformation
-from torch.distributed._spmd.graph_optimization import (
-    _optimized_func,
+from torch.distributed._spmd.graph_transformations import (
     comm_fusion_with_concat,
     find_all_descendants,
     get_all_fused_optimizer_blocks,
@@ -21,6 +20,7 @@ from torch.distributed._spmd.graph_optimization import (
     schedule_comm_wait,
     split_fused_optimizer,
 )
+from torch.distributed._spmd.graph_transformations.common import _optimized_func
 from torch.distributed._spmd.graph_utils import find_node
 from torch.distributed._spmd.iter_graph_module import IterGraphModule
 from torch.nn.parallel import DistributedDataParallel as DDP
---
+++ torch/distributed/_spmd/graph_transformations/__init__.py
@@ -0,0 +1,17 @@
+from .comm_fusion import comm_fusion_with_concat, schedule_comm_wait
+from .common import (
+    CommBlock,
+    enable_graph_optimization_dump,
+    get_all_comm_blocks,
+    get_comm_block,
+    graph_optimization_pass,
+)
+from .optimizer import (
+    AdamArgs,
+    find_all_descendants,
+    FusedAdamBlock,
+    get_all_fused_optimizer_blocks,
+    iter_move_grads_and_optimizers,
+    remove_copy_from_optimizer,
+    split_fused_optimizer,
+)
