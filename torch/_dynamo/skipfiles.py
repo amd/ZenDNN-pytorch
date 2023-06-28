4b6d4d95ed8ccfd02865130c8037515e6cc126f4
Michael Voznesensky <voznesenskym@gmail.com>
2023-06-28 06:57:57 +0000

    FSDP skipfiles, allows, extensions for dynamo
    FSDP skipfiles, allows, extensions for dynamo
    
    ghstack-source-id: 207acef668f68d48555a39241c60ccdd218eaa6d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/104211
    
    FSDP skipfiles, allows, extensions for dynamo
    
    FSDP skipfiles, allows, extensions for dynamo
    
    FSDP skipfiles, allows, extensions for dynamo
    
    FSDP skipfiles, allows, extensions for dynamo
    
    Fix
    
    Fix
    
    Fix
    
    Fix
    
    Fix
    
    Fix
    
    Fix
    
    Fix
    
    Fix

--- torch/_dynamo/skipfiles.py
+++ torch/_dynamo/skipfiles.py
@@ -32,7 +32,7 @@ import weakref
 
 import torch
 import torch._inductor.test_operators
-import torch.distributed
+import torch.nn.parallel.scatter_gather
 import torch.utils._content_store
 
 from . import comptime, config, external_utils
@@ -132,6 +132,68 @@ FILENAME_ALLOWLIST |= {
 }
 FILENAME_ALLOWLIST |= {torch.optim._functional.__file__}
 FILENAME_ALLOWLIST |= {torch.utils._foreach_utils.__file__}
+
+if torch.distributed.is_available():
+    import torch.distributed.fsdp
+    import torch.distributed.utils
+
+    FILENAME_ALLOWLIST |= {
+        inspect.getfile(obj)
+        for obj in torch.distributed.fsdp.__dict__.values()
+        if inspect.isclass(obj)
+    }
+
+    FILENAME_ALLOWLIST |= {inspect.getfile(torch.distributed.fsdp._utils._same_storage)}
+
+    FILENAME_ALLOWLIST |= {
+        inspect.getfile(obj)
+        for obj in torch.distributed.fsdp._exec_order_utils.__dict__.values()
+        if inspect.isclass(obj)
+    }
+
+    FILENAME_ALLOWLIST |= {inspect.getfile(torch.nn.parallel.scatter_gather.scatter)}
+
+    FILENAME_ALLOWLIST |= {
+        inspect.getfile(torch.distributed._composable_state._insert_module_state)
+    }
+
+    FILENAME_ALLOWLIST |= {
+        inspect.getfile(obj)
+        for obj in torch.distributed.fsdp._fsdp_extensions.__dict__.values()
+        if inspect.isclass(obj)
+    }
+
+    FILENAME_ALLOWLIST |= {
+        inspect.getfile(obj)
+        for obj in torch.distributed.fsdp._traversal_utils.__dict__.values()
+        if inspect.isclass(obj) or inspect.isfunction(obj)
+    }
+
+    FILENAME_ALLOWLIST |= {
+        inspect.getfile(obj)
+        for obj in torch.distributed._composable.contract.__dict__.values()
+        if inspect.isclass(obj) or inspect.isfunction(obj)
+    }
+
+    FILENAME_ALLOWLIST |= {
+        inspect.getfile(torch.distributed.fsdp._runtime_utils._get_fsdp_root_states_with_modules)
+    }
+
+    FILENAME_ALLOWLIST |= {
+        inspect.getfile(torch.distributed.utils._alloc_storage)
+    }
+
+    FILENAME_ALLOWLIST |= {
+        inspect.getfile(torch.distributed.distributed_c10d.all_gather_into_tensor)
+    }
+
+    FILENAME_ALLOWLIST |= {
+        inspect.getfile(torch.distributed.distributed_c10d.all_gather)
+    }
+
+    FILENAME_ALLOWLIST |= {inspect.getfile(torch.distributed.is_available)}
+
+    FILENAME_ALLOWLIST |= {inspect.getfile(torch.distributed.is_initialized)}
 
 # Do trace through match and replace patterns used in PT2E QAT
 # Note: These patterns are comprised of torch ops and for internal use only.
--- torch/_dynamo/utils.py
+++ torch/_dynamo/utils.py
@@ -486,7 +486,7 @@ def proxy_args_kwargs(args, kwargs):
         from .variables.base import typestr
 
         raise unimplemented(
-            f"call_function args: {typestr(*args)} {typestr(*list(kwargs.values()))}"
+            f"Tried to get proxy to call_function with args: {typestr(*args)} kwargs: {typestr(*list(kwargs.values()))}"
         ) from e
 
 
@@ -1705,7 +1705,9 @@ def higher_order_op_converter():
 
 
 def requires_higher_order_op(obj):
-    return obj in higher_order_op_converter()
+    return (
+        isinstance(obj, collections.abc.Hashable) and obj in higher_order_op_converter()
+    )
 
 
 def get_higher_order_op(obj):
