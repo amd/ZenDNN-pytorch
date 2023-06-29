8d16d91ae2641fdf36cd69cb5ca1891bdb7ccb16
Nicolas Macchioni <nmacchioni@meta.com>
2023-06-28 22:20:17 -0700

    [pt2][inductor] include `allow_tf32` in system information (#104129)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/104129
    
    include `allow_tf32` in system information; previously aten results did not specify whether `allow_tf32` was true or not
    
    Test Plan: sandcastle + CI
    
    Reviewed By: jansel
    
    Differential Revision: D46568468
    
    fbshipit-source-id: 437347f3df1c2a962937ce0bd745b625f55c5328

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -116,6 +116,9 @@ class CacheBase:
                 "cuda": torch.version.cuda,
                 "triton": triton_version,
             },
+            "other": {
+                "allow_tf32": torch.backends.cuda.matmul.allow_tf32,
+            },
         }
 
         system["hash"] = hashlib.sha256(
