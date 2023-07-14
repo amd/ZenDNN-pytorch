1ba4f4bad3d80b29baf4267746c946a0469915bd
Kamalanabhan Janarthanam <kamalanabhan.janarthanam@amd.com>
2023-07-14 09:24:43 +0000

    [ZENDNN FWK] Python Model Converter Support in PyTorch+ZenDNN
    - Retains fake_quantize_per_tensor_affine nodes during
    constant propagation pass.
    - Support required for Python PyTorch Model Converter
    
    Signed-off-by: Kamalanabhan Janarthanam <kamalanabhan.janarthanam@amd.com>
    Change-Id: Icb58c196517ba489710da1c49bd72727ac4c6802
    Reviewed-on: https://gerrit-git.amd.com/c/amd/ec/ZenDNN_PyTorch/+/891631
    Reviewed-by: Dinesh Mareedu <Dinesh.Mareedu@amd.com>
    Reviewed-by: Chandra Kumar Ramasamy <ChandraKumar.Ramasamy@amd.com>
    Tested-by: Llvm Cpu <llvm.cpu@amd.com>

--- torch/csrc/jit/passes/constant_propagation.cpp
+++ torch/csrc/jit/passes/constant_propagation.cpp
@@ -1,5 +1,36 @@
+/******************************************************************************
++* Modifications Copyright (c) 2023 Advanced Micro Devices, Inc.
++* All rights reserved.
++*
++* Redistribution and use in source and binary forms, with or without
++* modification, are permitted provided that the following conditions are met:
++*
++* 1. Redistributions of source code must retain the above copyright notice,
++* this list of conditions and the following disclaimer.
++* 2. Redistributions in binary form must reproduce the above copyright notice,
++* this list of conditions and the following disclaimer in the documentation
++* and/or other materials provided with the distribution.
++* 3. Neither the name of the copyright holder nor the names of its contributors
++* may be used to endorse or promote products derived from this software without
++* specific prior written permission.
++*
++* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
++* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
++* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
++* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
++* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
++* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
++* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
++* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
++* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
++* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
++* POSSIBILITY OF SUCH DAMAGE.
++*
++******************************************************************************/
+
 #include <torch/csrc/jit/passes/constant_propagation.h>
 
+#include <ATen/Config.h>
 #include <ATen/core/functional.h>
 #include <ATen/core/ivalue.h>
 #include <c10/util/Exception.h>
@@ -367,6 +398,12 @@ struct ConstantPropagator {
   }
 
   void ConstantPropagation(Node* n) {
+    #if AT_ZENDNN_QUANT_ENABLED()
+    if (n->kind() == aten::fake_quantize_per_tensor_affine) {
+      return;
+      }
+    #endif
+
     bool constant_inputs =
         std::all_of(n->inputs().begin(), n->inputs().end(), [&](Value* v) {
           return v->node()->kind() == prim::Constant;
